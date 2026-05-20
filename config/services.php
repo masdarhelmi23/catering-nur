f8dc34790098b81b369aabae9e4b2fa20cf984bf
Masdar Helmi <masdarhelmi2301@gmail.com>
2026-05-20 22:07:12 +0700

    Fix: Sembunyikan credential sensitif

--- config/services.php
+++ config/services.php
@@ -36,9 +36,9 @@ return [
     ],
 
     'google' => [
-        'client_id' => '700085927053-gignlj2n87ghkt0q7a78ip3p2qlmr3l6.apps.googleusercontent.com',
-        'client_secret' => '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',
-        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
     ],
 
 ];
